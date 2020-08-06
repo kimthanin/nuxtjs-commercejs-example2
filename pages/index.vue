702d6ca8067f7b33bbf2f29bd2d5f23c59f2e569
Thanin Wangveerathananon <me@thanin.kim>
2020-08-06 14:05:31 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ merchant.business_name }}</h1>
+
+    <h3>
+      <n-link to="/categories">Categories</n-link>
+    </h3>
+
+    <category-list :categories="categories"></category-list>
+
+    <h3>
+      <n-link to="/products">Products</n-link>
+    </h3>
+
+    <product-list :products="products"></product-list>
+  </div>
+</template>
+
+<script>
+import commerce from "~/common/commerce";
+
+export default {
+  async asyncData() {
+    const merchant = await commerce.merchants.about();
+    const { data: categories } = await commerce.categories.list();
+    const { data: products } = await commerce.products.list();
+
+    return {
+      merchant,
+      categories,
+      products,
+    };
+  },
+};
+</script>
